890911f3b6d539ce762c08ec656b26757f16d6d1
Connie Chan <conniechan.10@gmail.com>
2019-04-03 23:39:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ function fetchCurrentWeather(city) {
 
 function displayCurrentWeather(json) {
   //render current weather data to the DOM using provided IDs and json from API
-  const currentTemp = json.main.temp
+  // const currentTemp = json.main.temp
   const tempContainer = document.getElementById('temp')
-  tempContainer.innerHTML = currentTemp
+  tempContainer.innerHTML = json.main.temp
 }
 
 
