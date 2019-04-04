c6b05fb98da22c8844c79bbaf21eea6b19a52e96
Connie Chan <conniechan.10@gmail.com>
2019-04-04 00:08:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -44,7 +44,10 @@ function fetchFiveDayForecast(city) {
 function displayFiveDayForecast(json) {
   //render five day forecast data to the DOM using provided IDs and json from API
   console.log(json)
-  for (let forecast of forecasts)
+  const forecast = json.list
+  for (let forecast of forecasts) {
+    console.log(forecast)
+  }
 }
 
 function createChart(json) {
